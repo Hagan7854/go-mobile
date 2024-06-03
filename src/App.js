cb09c1d8aff9da05d9bfb5ca7cfba084d0855a1a
Hagan7854 <Haganfelix43@gmail.com>
2024-06-03 12:29:56 +0000

    made alterations in the app and landing-page css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import LandingPage from "./component/landing-page/landing-page";
 
 function App() {
   return (
     <div className="App">
-      <div className="bg-image"></div>
+      <div className="landing-page">
+        <LandingPage />
+      </div>
     </div>
   );
 }
--- src/component/landing-page/landing-page.jsx
+++ src/component/landing-page/landing-page.jsx
@@ -1,3 +1,38 @@
+import "./landing-page.css";
+import phoneImage from "./images/app-concept.jpg";
+import logo from "./images/logo.png";
+import playStoreImage from "./images/PLAYSTORE.png";
+import appStoreImage from "./images/APPLE SOTR.png";
+
 export default function LandingPage() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="header">
+        <nav>
+          <a href="#app">The app</a>
+          <a href="#login">Login</a>
+          <a href="#communites">Communities</a>
+          <a href="#about">About</a>
+        </nav>
+      </div>
+      <div className="row">
+        <div className="col-1">
+          <img src={phoneImage} alt="" />
+        </div>
+        <div className="col-2">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="sub-titles">
+            <h1>Mobile App</h1>
+            <p>Get the App</p>
+          </div>
+          <div className="app">
+            <img src={playStoreImage} alt="" />
+            <img src={appStoreImage} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

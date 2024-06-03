98a185be15cb4e18d396f51899efdb265a9dd2db
Hagan7854 <Haganfelix43@gmail.com>
2024-06-03 14:47:59 +0000

    altered the logo and navbar

--- src/component/pages/authentication/login.jsx
+++ src/component/pages/authentication/login.jsx
@@ -1,11 +1,26 @@
 import logo from "./image/logo.png";
+import padlock from "./image/padlock.svg";
+import menuIcon from "./image/menu.svg";
 import "./login.css";
 
 export default function Login() {
   return (
     <div className="background-login">
       <div className="header">
-        <div className="logo">{/* <img src={logo} alt="Go logo" /> */}</div>
+        <div className="logo">
+          <img src={logo} alt="Go logo" />
+        </div>
+        <div className="nav">
+          <div className="icon">
+            <img src={padlock} alt="" />
+          </div>
+          <div className="nav-text">
+            <p>Login</p>
+          </div>
+          <div className="icon">
+            <img src={menuIcon} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );

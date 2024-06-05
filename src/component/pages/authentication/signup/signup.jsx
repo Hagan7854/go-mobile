928e938be3fd47b93b7c67970a41f418e7f5deca
Hagan7854 <Haganfelix43@gmail.com>
2024-06-05 16:18:21 +0000

    took out the contents of the css file

--- src/component/pages/authentication/signup/signup.jsx
+++ src/component/pages/authentication/signup/signup.jsx
@@ -8,64 +8,44 @@ export default function Signup() {
       <div className="signup-body">
         <div className="frame">
           <form>
-            <div className="sign-up-input">
-              <div className="label">
-                <label htmlFor="sigUpName">Name</label>
-              </div>
-              <div className="wrapper">
-                <input
-                  type="text"
-                  id="signUpName"
-                  name="name"
-                  placeholder="enter username"
-                />
-                <img src={person} alt="" />
-              </div>
+            <div className="wrapper">
+              <input
+                type="text"
+                id="signUpName"
+                name="name"
+                placeholder="enter username"
+              />
+              <img src={person} alt="" />
             </div>
-            <div className="sign-up-input spacing">
-              <div className="label">
-                <label htmlFor="sigUpEmail">Email</label>
-              </div>
-              <div className="wrapper">
-                <input
-                  type="email"
-                  id="signUpEmail"
-                  name="Email"
-                  required
-                  placeholder="example@.x.com"
-                />
-                <img src={person} alt="" />
-              </div>
+            <div className="wrapper">
+              <input
+                type="email"
+                id="signUpEmail"
+                name="Email"
+                required
+                placeholder="example@.x.com"
+              />
+              <img src={person} alt="" />
             </div>
-            <div className="sign-up-input spacing">
-              <div className="label">
-                <label htmlFor="sigUpPassword">Password</label>
-              </div>
-              <div className="wrapper password">
-                <input
-                  type="password"
-                  id="signUpPassword"
-                  name="password"
-                  placeholder="●●●●●●●●"
-                  required
-                />
-                <img src={passwordIcon} alt="" />
-              </div>
+            <div className="wrapper">
+              <input
+                type="password"
+                id="signUpPassword"
+                name="password"
+                placeholder="●●●●●●●●"
+                required
+              />
+              <img src={passwordIcon} alt="" />
             </div>
-            <div className="sign-up-input spacing">
-              <div className="label">
-                <label htmlFor="confirmPassword">Confirm Password</label>
-              </div>
-              <div className="wrapper confirm-password">
-                <input
-                  type="password"
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  placeholder="●●●●●●●●"
-                  required
-                />
-                <img src={passwordIcon} alt="" />
-              </div>
+            <div className="wrapper">
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                placeholder="●●●●●●●●"
+                required
+              />
+              <img src={passwordIcon} alt="" />
             </div>
             <div className="signup-button btn">
               <button>Sign Up</button>

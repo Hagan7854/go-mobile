dd6b6e356d33b4a7029c44774a0bb9208290465f
Hagan7854 <Haganfelix43@gmail.com>
2024-06-05 12:55:12 +0000

    trying to align the input fields

--- src/component/pages/authentication/signup/signup.jsx
+++ src/component/pages/authentication/signup/signup.jsx
@@ -1,66 +1,74 @@
 import "./signup.css";
+import person from "../login/image/username.svg";
+import passwordIcon from "../login/image/password.svg";
 
 export default function Signup() {
   return (
     <div>
       <div className="signup-body">
-        <form>
-          <div className="sign-up-input">
-            <div>
-              <label htmlFor="sigUpName">Name</label>
+        <div className="frame">
+          <form>
+            <div className="sign-up-input">
+              <div>
+                <label htmlFor="sigUpName">Name</label>
+              </div>
+              <div className="wrapper">
+                <input
+                  type="text"
+                  id="signUpName"
+                  name="name"
+                  placeholder="enter username"
+                />
+                <img src={person} alt="" />
+              </div>
             </div>
-            <div className="wrapper">
-              <input
-                type="text"
-                id="signUpName"
-                name="name"
-                placeholder="enter username"
-              />
+            <div className="sign-up-input spacing">
+              <div>
+                <label htmlFor="sigUpEmail">Email</label>
+              </div>
+              <div className="wrapper">
+                <input
+                  type="email"
+                  id="signUpEmail"
+                  name="Email"
+                  required
+                  placeholder="example@.x.com"
+                />
+                <img src={person} alt="" />
+              </div>
             </div>
-          </div>
-          <div className="sign-up-input spacing">
-            <div>
-              <label htmlFor="sigUpEmail">Email</label>
+            <div className="sign-up-input spacing">
+              <div>
+                <label htmlFor="sigUpPassword">Password</label>
+              </div>
+              <div className="wrapper">
+                <input
+                  type="password"
+                  id="signUpPassword"
+                  name="password"
+                  required
+                />
+                <img src={passwordIcon} alt="" />
+              </div>
             </div>
-            <div className="wrapper">
-              <input
-                type="email"
-                id="signUpEmail"
-                name="Email"
-                required
-                placeholder="example@.x.com"
-              />
+            <div className="sign-up-input spacing">
+              <div>
+                <label htmlFor="confirmPassword">Confirm Password</label>
+              </div>
+              <div className="wrapper">
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                />
+                <img src={passwordIcon} alt="" />
+              </div>
             </div>
-          </div>
-          <div className="sign-up-input spacing">
-            <div>
-              <label htmlFor="sigUpPassword">Password</label>
+            <div className="signup-button">
+              <button>Sign Up</button>
             </div>
-            <div className="wrapper">
-              <input
-                type="password"
-                id="signUpPassword"
-                name="password"
-                required
-              />
-            </div>
-          </div>
-          <div className="sign-up-input spacing">
-            <div>
-              <label htmlFor="confirmPassword">Confirm Password</label>
-            </div>
-            <div className="wrapper">
-              <input
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-              />
-            </div>
-          </div>
-          <div className="signup-button">
-            <button>Sign Up</button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );

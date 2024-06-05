e3b64c51c7be6fed7257ccc43924315e598f2002
Hagan7854 <Haganfelix43@gmail.com>
2024-06-05 10:10:18 +0000

    moved the login.jsx and login.css into a newly created login folder and also created a sigup folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import LandingPage from "./component/landing-page/landing-page";
-import Login from "./component/pages/authentication/login";
+import Login from "./component/pages/authentication/login/login";
 
 function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
-
   // return (
   //   <div className="App">
-  //     <Login />
+  //     <LandingPage />
   //   </div>
   // );
+
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;

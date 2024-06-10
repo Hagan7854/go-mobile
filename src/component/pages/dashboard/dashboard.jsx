095461e4c8db87c888390fdbb204a3a05cf8f4db
Hagan7854 <Haganfelix43@gmail.com>
2024-06-10 15:31:03 +0000

    added a sidebar menu

--- src/component/pages/dashboard/dashboard.jsx
+++ src/component/pages/dashboard/dashboard.jsx
@@ -5,6 +5,16 @@ import searchIcon from "../authentication/login/image/search.svg";
 import notification from "../authentication/login/image/bell.svg";
 import padlockIcon from "../authentication/login/image/padlock.svg";
 import prorilePic from "../authentication/login/image/profile-pic.svg";
+import { Sidebar } from "flowbite-react";
+import {
+  HiArrowSmRight,
+  HiChartPie,
+  HiInbox,
+  HiShoppingBag,
+  HiTable,
+  HiUser,
+  HiViewBoards,
+} from "react-icons/hi";
 
 export default function Dashboard() {
   return (
@@ -36,6 +46,43 @@ export default function Dashboard() {
             <img src={padlockIcon} alt="" />
           </div>
         </div>
+      </div>
+      <div>
+        <Sidebar aria-label="Default sidebar example" className="sidebar">
+          <Sidebar.Logo href="#" img="/favicon.svg" imgAlt="Flowbite logo">
+            Flowbite
+          </Sidebar.Logo>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item
+                href="#"
+                icon={HiViewBoards}
+                label="Pro"
+                labelColor="dark"
+              >
+                Kanban
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox} label="3">
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
       </div>
     </div>
   );

0e6a5df64a8872b81688ff5eb79b92328cf33372
Hagan7854 <Haganfelix43@gmail.com>
2024-06-11 14:30:35 +0000

    added new content to the main side

--- src/component/pages/dashboard/dashboard.jsx
+++ src/component/pages/dashboard/dashboard.jsx
@@ -6,15 +6,10 @@ import notification from "../authentication/login/image/bell.svg";
 import padlockIcon from "../authentication/login/image/padlock.svg";
 import prorilePic from "../authentication/login/image/profile-pic.svg";
 import { Sidebar } from "flowbite-react";
-import {
-  HiArrowSmRight,
-  HiChartPie,
-  HiInbox,
-  HiShoppingBag,
-  HiTable,
-  HiUser,
-  HiViewBoards,
-} from "react-icons/hi";
+import communitiesIcon from "../../landing-page/images/communities.svg";
+import membersIcon from "../../landing-page/images/members.svg";
+import message from "../../landing-page/images/messages.svg";
+import activities from "../../landing-page/images/aechives.svg";
 
 export default function Dashboard() {
   return (
@@ -47,39 +42,68 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-      <div>
-        <Sidebar
-          aria-label="Sidebar with logo branding example"
-          className="sidebar"
-        >
-          <Sidebar.Logo
-            href="#"
-            img={prorilePic}
-            imgAlt="Flowbite logo"
-            className="flex nav-gap"
+      <div className="content">
+        <div className="frame-1">
+          <Sidebar
+            aria-label="Sidebar with logo branding example"
+            className="sidebar"
           >
-            <p className="profile-name">Ken Cann</p>
-          </Sidebar.Logo>
-          <Sidebar.Items>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item href="#" icon={HiChartPie} className="nav-gap">
-                <p className="profile-name">My Communtiies</p>
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiViewBoards} className="nav-gap">
-                <p className="profile-name">Members</p>
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiInbox} className="nav-gap">
-                <p className="profile-name">Messages</p>
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiUser} className="nav-gap">
-                <p className="profile-name">Activities</p>
-              </Sidebar.Item>
-              <Sidebar.Item>
-                <button className="sidebar-button">New Communities</button>
-              </Sidebar.Item>
-            </Sidebar.ItemGroup>
-          </Sidebar.Items>
-        </Sidebar>
+            <div className="profile-wrapper">
+              <div className="profile-pic">
+                <img src={prorilePic} alt="" />
+              </div>
+              <div className="profile-txt">
+                <p className="profile-name">Ken Cann</p>
+                <p className="sub-profile-name">@ncann</p>
+              </div>
+            </div>
+            <Sidebar.Items>
+              <Sidebar.ItemGroup>
+                <a href="#">
+                  <div className="nav-gap">
+                    <img src={communitiesIcon} alt="" />
+                    <p className="sidebar-items">My Communtiies</p>
+                  </div>
+                </a>
+                <a href="##">
+                  <div className="nav-gap">
+                    <img src={membersIcon} alt="" />
+                    <p className="sidebar-items">Members</p>
+                  </div>
+                </a>
+                <a href="#">
+                  <div className="nav-gap">
+                    <img src={message} alt="" />
+                    <p className="sidebar-items">Message</p>
+                  </div>
+                </a>
+                <a href="#">
+                  <div className="nav-gap">
+                    <img src={activities} alt="" />
+                    <p className="sidebar-items">Activities</p>
+                  </div>
+                </a>
+
+                <Sidebar.Item>
+                  <button className="sidebar-button">New Communities</button>
+                </Sidebar.Item>
+              </Sidebar.ItemGroup>
+            </Sidebar.Items>
+          </Sidebar>
+        </div>
+        <div className="frame-2">
+          <p className="title">My communities</p>
+          <div className="main">
+            <div className="first-frame">
+              <div className="pic">
+                <img src="" alt="picture" />
+              </div>
+              <div className="stats">
+                <p>FITNESS</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

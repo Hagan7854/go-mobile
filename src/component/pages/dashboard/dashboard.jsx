c720d5e52e5cc880ff81fc839cb8f251a7b6c890
Hagan7854 <Haganfelix43@gmail.com>
2024-06-12 20:51:11 +0000

    done with the card

--- src/component/pages/dashboard/dashboard.jsx
+++ src/component/pages/dashboard/dashboard.jsx
@@ -9,8 +9,11 @@ import { Sidebar } from "flowbite-react";
 import communitiesIcon from "../../landing-page/images/communities.svg";
 import membersIcon from "../../landing-page/images/members.svg";
 import message from "../../landing-page/images/messages.svg";
-import activities from "../../landing-page/images/aechives.svg";
+import activities from "../../landing-page/images/activities.svg";
 import running from "../../landing-page/images/outdoor-shot-active-dark-skinned-man-running-morning-has-regular-trainings-dressed-tracksuit-comfortable-sneakers-concentrated-into-distance-sees-finish-far-away.jpg";
+import communityIcon from "../../landing-page/images/communityIcon.svg";
+import clockIcon from "../../landing-page/images/clock.svg";
+import StarIcon from "../../landing-page/images/193-1934674_star-symbol-text-star-icon-transparent-background.webp";
 
 export default function Dashboard() {
   return (
@@ -104,6 +107,46 @@ export default function Dashboard() {
                     <button>OPEN</button>
                   </div>
                 </div>
+              </div>
+              <div className="second-row">
+                <div className="second-col">
+                  <div className="communityIcon">
+                    <img src={communityIcon} alt="" />
+                  </div>
+                  <div className="members">
+                    <p>47</p>
+                    <p>Members</p>
+                  </div>
+                </div>
+                <div className=" duration">
+                  <img src={clockIcon} alt="" />
+                  <p>1 years old</p>
+                </div>
+              </div>
+              <div className="rating">
+                <img src={StarIcon} alt="" />
+                <img src={StarIcon} alt="" />
+                <img src={StarIcon} alt="" />
+                <img src={StarIcon} alt="" />
+                <img src={StarIcon} alt="" />
+                <img src={StarIcon} alt="" />
+              </div>
+              <div className="upcoming-event">
+                <h1>UPCOMING EVENT</h1>
+              </div>
+              <div className="last-row">
+                <div className="date">
+                  <p>2024.05.27</p>
+                </div>
+                <div className="sessions">
+                  <p>Physio</p>
+                  <p>Session</p>
+                  <p>Lamba Court</p>
+                  <p>Spintex</p>
+                </div>
+              </div>
+              <div className="bar-wrapper">
+                <div className="bar"></div>
               </div>
             </div>
           </div>

587358478a89bf5b79c94be33a90ffcb07f7541d
Hagan7854 <Haganfelix43@gmail.com>
2024-06-23 17:55:29 +0000

    finished with the categories and sub-heading

--- src/component/pages/dashboard/Community/Communty.jsx
+++ src/component/pages/dashboard/Community/Communty.jsx
@@ -3,8 +3,15 @@ import "../Community/Community.css";
 export default function Community() {
   return (
     <div className="community">
-      <div className="categories">
-        <div className="category"></div>
+      <div className="categories flex">
+        <div className="category">Arts</div>
+        <div className="category">Games</div>
+        <div className="category">Fitness</div>
+        <div className="category">Singing</div>
+        <div className="category">Dance</div>
+      </div>
+      <div>
+        <p className="heading-communties">Communities</p>
       </div>
     </div>
   );

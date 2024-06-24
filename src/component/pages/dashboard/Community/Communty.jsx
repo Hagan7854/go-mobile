e44c666e25eaa53fae86384b9de8e4e832efb0d7
Hagan7854 <Haganfelix43@gmail.com>
2024-06-24 09:14:41 +0000

    added an image to the community-wrapper class

--- src/component/pages/dashboard/Community/Communty.jsx
+++ src/component/pages/dashboard/Community/Communty.jsx
@@ -1,5 +1,6 @@
 import "../Community/Community.css";
-
+import imageOnTry from "../../../landing-page/images/full-shot-woman-stretching-outdoors.jpg";
+import icon from "../../../landing-page/images/communityIcon.svg";
 export default function Community() {
   return (
     <div className="community">
@@ -12,6 +13,22 @@ export default function Community() {
       </div>
       <div>
         <p className="heading-communties">Communities</p>
+      </div>
+      <div className="community-container">
+        <div className="community-widget">
+          <div className="community-image">
+            <img src={imageOnTry} alt="" />
+          </div>
+          <div className="community-text">
+            <div className="community-title">
+              <p>ART</p>
+            </div>
+            <div className="first-text">
+              <img src={icon} alt="" />
+              <p>47 Members</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

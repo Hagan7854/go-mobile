27e790721b3615ee422561d31c2a5f899d397a50
Hagan7854 <Haganfelix43@gmail.com>
2024-06-24 12:03:10 +0000

    done with community widget class

--- src/component/pages/dashboard/Community/Communty.jsx
+++ src/component/pages/dashboard/Community/Communty.jsx
@@ -1,6 +1,9 @@
 import "../Community/Community.css";
 import imageOnTry from "../../../landing-page/images/full-shot-woman-stretching-outdoors.jpg";
 import icon from "../../../landing-page/images/communityIcon.svg";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Community() {
   return (
     <div className="community">
@@ -11,7 +14,7 @@ export default function Community() {
         <div className="category">Singing</div>
         <div className="category">Dance</div>
       </div>
-      <div>
+      <div className="community-div">
         <p className="heading-communties">Communities</p>
       </div>
       <div className="community-container">
@@ -20,12 +23,34 @@ export default function Community() {
             <img src={imageOnTry} alt="" />
           </div>
           <div className="community-text">
-            <div className="community-title">
-              <p>ART</p>
+            <div className="title-wrapper">
+              <div className="community-title">
+                <p>ART</p>
+              </div>
+              <div className="first-text">
+                <img src={icon} alt="" />
+                <p>47 Members</p>
+              </div>
             </div>
-            <div className="first-text">
-              <img src={icon} alt="" />
-              <p>47 Members</p>
+            <div className="green-text">
+              <h3>The Creative Canvas</h3>
+              <div className="star-rating">
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+            </div>
+            <div className="read-more">
+              <p>
+                Creative Canvas is a vibrant community of artists dedicated to
+                exploring and expanding the boundaries of contemporary art.
+                Founded in 2015, this group brings together painters, sculptors,
+                digital artists, and mixed-media creators who are passionate
+                about collaboration and innovation.
+              </p>
+              <button className="join">join +</button>
             </div>
           </div>
         </div>
